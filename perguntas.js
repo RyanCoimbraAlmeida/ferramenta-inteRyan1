aa055b8298db41899db12b07bd547bf9b5499a30
gomaplys <133662473+RyanCoimbraAlmeida@users.noreply.github.com>
2024-12-03 20:55:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é um número natural maior que 1 que não pode ser formado pela multiplicação de dois números inteiros menores que ele.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Napoleão Bonaparte foi um líder militar e imperador francês, conhecido por suas conquistas na Europa no início do século XIX.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'A fotossíntese é o processo pelo qual as plantas convertem luz solar em energia, usando dióxido de carbono e água.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a gravidade?',
+    'A gravidade é a força que atrai objetos para o centro da Terra, responsável pela queda dos corpos e pelo movimento dos planetas.'
+)
